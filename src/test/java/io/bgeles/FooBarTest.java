86b4555c60f7066d6faf34cbde825da007314c41
Bruno Glauco Eles <bgeles@gmail.com>
2024-07-05 18:41:11 -0300

    add tests

---
+++ src/test/java/io/bgeles/FooBarTest.java
@@ -0,0 +1,14 @@
+package io.bgeles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+	@Test
+	void test() {
+//		fail("Not yet implemented");
+	}
+
+}
